ea48c8ff2898e0b9746b030067dab494e5443219
lwang130 <lwang130@illinois.edu>
2017-11-28 17:03:54 -0600

    Add files via upload

--- app/src/main/java/edu/illinois/handsup/MainActivity.java
+++ app/src/main/java/edu/illinois/handsup/MainActivity.java
@@ -1,13 +1,32 @@
-package edu.illinois.handsup;
+package com.example.wlc.handsup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout androidDropDownMenuIconItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        androidDropDownMenuIconItem = (LinearLayout) findViewById(R.id.horizontal_dropdown_icon_menu_items);
+    }
+
+    public void horizontalDropDownIconMenu(View view) {
+        if (androidDropDownMenuIconItem.getVisibility() == View.VISIBLE) {
+            androidDropDownMenuIconItem.setVisibility(View.INVISIBLE);
+        } else {
+            androidDropDownMenuIconItem.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void menuItemClick(View view) {
+        androidDropDownMenuIconItem.setVisibility(View.INVISIBLE);
     }
 }
